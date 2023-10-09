941644a94f96a99995278ea561a3599c98a3478d
ronan peixoto <ronan_peixoto@hotmail.com>
2023-10-09 16:34:27 -0300

    edit requirements.txt

--- Fakepinterest/__init__.py
+++ Fakepinterest/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "f0f0ccade9f144f299f3ee5e1fe55f21"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
